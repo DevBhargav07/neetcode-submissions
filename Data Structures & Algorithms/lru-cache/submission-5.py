2614e39af2747a657d0f041b18958d4adc88c0b9
DevBhargav07 <bhargavdevarampati24@gmail.com>
2026-08-03 17:54:13 +0530

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.py
@@ -0,0 +1,23 @@
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.data = OrderedDict()
+
+    def get(self, key: int) -> int:
+        res = self.data.get(key, -1)
+        if res != -1:
+            self.data.move_to_end(key)
+        return res
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.data:
+            self.data.move_to_end(key)
+        elif len(self.data) >= self.capacity:
+            self.data.popitem(last=False)
+        self.data[key] = value
+        
+
+        
