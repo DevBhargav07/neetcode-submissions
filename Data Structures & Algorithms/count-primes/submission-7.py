50814d013db2c170c6aa2f640357704a112f9bd8
Bhargav D <249870767+DevBhargav07@users.noreply.github.com>
2026-09-23 16:47:20 +0530

    Add: count-primes - submission-7

---
+++ Data Structures & Algorithms/count-primes/submission-7.py
@@ -0,0 +1,38 @@
+class Solution:
+    # def is_prime(self, n: int) -> bool:
+    #     for i in range(2, n//2+1):
+    #         if n % i == 0:
+    #             return False
+    #     return True
+
+    def countPrimes(self, n: int) -> int:
+        # count = 0
+        # for num in range(2, n):
+        #     if self.is_prime(num):
+        #         count += 1
+        # return count
+
+        #sieve of Eratosthenes
+        # if n <= 2: return 0
+        
+        # sieve = [False] * n
+        # res = 0
+        # for num in range(2, n):
+        #     if not sieve[num]:
+        #         res += 1
+        #         for i in range(num * num, n, num):
+        #             sieve[i] = True
+        # return res
+
+
+        if n <= 2: return 0
+
+        sieve = [True] * n
+        sieve[0] = sieve[1] = False
+
+        i = 2
+        while i * i < n:
+            if sieve[i]:
+                sieve[i * i:n:i] = [False] * (((n - 1 - i * i) // i) + 1)
+            i += 1
+        return sum(sieve)
